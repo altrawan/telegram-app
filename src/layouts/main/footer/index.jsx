58b0668129c10d522aead6ab114c6ea43f5a943c
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-06-06 14:12:00 +0700

    add feature input emoji

--- src/layouts/main/footer/index.jsx
+++ src/layouts/main/footer/index.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IconPlus, IconEmoticon, IconCamera } from '../../../assets/icons';
+import data from '@emoji-mart/data';
 import { Picker } from 'emoji-mart';
-import 'emoji-mart/css/emoji-mart.css';
 import './index.scss';
+
+function EmojiPicker(props) {
+  const ref = React.useRef();
+
+  useEffect(() => {
+    new Picker({ ...props, data, ref });
+  }, []);
+
+  return <div ref={ref} />;
+}
 
 const index = ({ onSendMessage, message, setMessage }) => {
   const [showEmojis, setShowEmojis] = React.useState(false);
@@ -37,12 +47,12 @@ const index = ({ onSendMessage, message, setMessage }) => {
             <img src={IconCamera} alt="icon" />
           </div>
         </div>
-        {showEmojis && (
-          <div>
-            <Picker onSelect={addEmoji} />
-          </div>
-        )}
       </form>
+      {showEmojis && (
+        <div>
+          <EmojiPicker onEmojiSelect={addEmoji} />
+        </div>
+      )}
     </section>
   );
 };
